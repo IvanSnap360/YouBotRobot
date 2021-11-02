f8e9d36cbac95f8ab886fcd01da8af62bd749515
IvanSnap360 <ivpetr2003@gmail.com>
2021-11-02 21:15:50 +0300

    SYNS_FROM_ROBOT_PC

--- git_upload.sh
+++ git_upload.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-echo "UPLOAD TO GIT $(basename `git rev-parse --show-toplevel`)"
 echo "###############UPLOAD####################" >> git_log.log
+echo "UPLOAD TO GIT $(basename `git rev-parse --show-toplevel`)"
 date >> git_log.log
 git add -A >> git_log.log
 git commit -m "SYNS_FROM_ROBOT_PC" >> git_log.log
 git push >> git_log.log
 echo "############### DONE ####################" >> git_log.log
 echo "############### DONE ####################" 
+echo ""
 
