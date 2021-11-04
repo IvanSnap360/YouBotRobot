7ddd07cd317add4043686221e17a0271d986ec67
IvanSnap360 <ivpetr2003@gmail.com>
2021-11-04 12:53:12 +0300

    some fix

--- git_upload_manual.sh
+++ git_upload_manual.sh
@@ -4,7 +4,7 @@ printf "###############UPLOAD#################### \n" >> git_log.log
 printf "UPLOAD TO GIT $(basename `git rev-parse --show-toplevel`)\n"
 date >> git_log.log
 git add -A >> git_log.log
-git commit -m "{$x}" >> git_log.log
+git commit -m "$x" >> git_log.log
 git push >> git_log.log
 printf "############### DONE ####################\n" >> git_log.log
 printf "############### DONE ####################\n" 
