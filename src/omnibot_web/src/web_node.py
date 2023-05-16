a8e38e961423b43f07050189c9ec32345c51f067
IvanSnap360 <ivpetr2003@gmail.com>
2023-05-16 21:49:37 +0300

    web changes

--- src/omnibot_web/src/web_node.py
+++ src/omnibot_web/src/web_node.py
@@ -5,9 +5,30 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
-@app.route("/home.html")
+@app.route("/statisticks.html")
 def home():
-    return render_template('home.html')
+    return render_template('statisticks.html')
+
+@app.route("/navigation.html")
+def navigation():
+    return render_template('navigation.html')
+
+@app.route("/manipulator.html")
+def manipulator():
+    return render_template('manipulator.html')
+
+@app.route("/platform.html")
+def platfrom():
+    return render_template('platform.html')
+
+@app.route("/cartographer.html")
+def cartographer():
+    return render_template('cartographer.html')
+
+@app.route("/input_data")
+def input_data():
+    return '{"a":123,"b":333}';
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)  
+    app.run(host='0.0.0.0', port=8080, debug=False)  
