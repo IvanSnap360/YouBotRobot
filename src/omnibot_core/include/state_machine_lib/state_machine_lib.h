9ee0a5de4fe94d0d22de38ea0e3a485444b0964e
IvanSnap360 <ivpetr2003@gmail.com>
2023-06-02 08:43:51 +0300

    add state machine

---
+++ src/omnibot_core/include/state_machine_lib/state_machine_lib.h
@@ -0,0 +1,16 @@
+#ifndef __STATE_MACHINE_LIB_H__
+#define __STATE_MACHINE_LIB_H__
+#include <ros/ros.h>
+
+class STATE_MACHINE_LIB
+{
+private:
+    ros::NodeHandle *_nh;
+public:
+    STATE_MACHINE_LIB();
+    ~STATE_MACHINE_LIB();
+};
+
+
+
+#endif // __STATE_MACHINE_LIB_H__
---
+++ src/omnibot_core/src/state_machine_lib/state_machine_lib.cpp
@@ -0,0 +1,9 @@
+#include "state_machine_lib/state_machine_lib.h"
+
+STATE_MACHINE_LIB::STATE_MACHINE_LIB(/* args */)
+{
+}
+
+STATE_MACHINE_LIB::~STATE_MACHINE_LIB()
+{
+}
