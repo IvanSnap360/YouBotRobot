45cb933b6d0d04ecf59f6314d7092ab24ee99b81
customer_c <customer_c@isupport.semidrive.com>
2024-10-06 21:33:08 +0300

    add controllers and controller manager

--- src/omnibot_description/launch/omnibot_description.launch.py
+++ src/omnibot_description/launch/omnibot_description.launch.py
@@ -2,14 +2,15 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument,ExecuteProcess
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration,NotSubstitution,Command
+from launch.substitutions import LaunchConfiguration,NotSubstitution,Command,PathJoinSubstitution
 from launch.conditions import IfCondition
 from launch_ros.actions import Node
 import xacro
 
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     package_path = get_package_share_directory('omnibot_description')
@@ -28,7 +29,24 @@ def generate_launch_description():
     robot_description = xacro.process_file(urdf_path, mappings={'robot_namespace' : 'omnibot_robot',
         'use_manipulator' : 'False'}).toxml()
     
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('omnibot_description'),
+            'config',
+            'controller_manager.yaml',
+        ]
+    )
     
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_velocity_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'velocity_controller'],
+        output='screen'
+    )
 
     robot_state_pub = Node(
                 package="robot_state_publisher",
@@ -64,10 +82,21 @@ def generate_launch_description():
                 condition = IfCondition(rviz)
                 )
     
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="screen",
+    )
+    
     loader =  LaunchDescription()
+    loader.add_action(control_node)
     loader.add_action(robot_state_pub)
     loader.add_action(joint_state_pub)
     loader.add_action(joint_state_gui_pub)
     loader.add_action(rviz_node)
+    loader.add_action(load_joint_state_broadcaster)
+    loader.add_action(load_joint_velocity_controller)
+  
     return loader
 
